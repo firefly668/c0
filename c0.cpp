fa2a95907b1f188641d3bf4a37aa5f0145f96025
firefly668 <49812001+firefly668@users.noreply.github.com>
2020-11-27 20:32:26 +0800

    Update c0.cpp

--- c0.cpp
+++ c0.cpp
@@ -1985,14 +1985,14 @@ bool analyse(){//TODO: 写入文件
 }
 
 int main(int argc,char** argv){
-    //file = fopen(argv[1],"rt");
-    file = fopen("test.txt","rt");
+    file = fopen(argv[1],"rt");
+    //file = fopen("test.txt","rt");
     /*char c='\0';
     while((c=fgetc(file))!=EOF){
         printf("%c",c);
     }*/
-    //outFile = fopen(argv[3],"wt");
-    outFile = fopen("out.txt","wt");
+    outFile = fopen(argv[3],"wt");
+    //outFile = fopen("out.txt","wt");
     if(!analyse()) return -1;
     fclose(file);
     fclose(outFile);
